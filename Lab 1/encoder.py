f25554b9a38a2a2dcc037bd5d1f8339047451ee1
rahul-go <33994284+rahul-go@users.noreply.github.com>
2019-01-17 14:57:12 -0800

    Progress, with errors

--- Lab 1/encoder.py
+++ Lab 1/encoder.py
@@ -33,19 +33,37 @@ class Encoder:
     ## Constructor for encoder driver
     #
     #  Detailed info on encoder driver constructor
-    def __init__(self):
-        self.t8 = pyb.Timer(8, prescaler=0, period=65535)
-        self.t8.channel(1, pyb.Timer.ENC_AB, pin=pyb.Pin.board.PC6)
-        self.t8.channel(2, pyb.Timer.ENC_AB, pin=pyb.Pin.board.PC7)
+    def __init__(self, timer, chA, chB, pinA, pinB):
+        
+        # Set up encoder pins
+        self.timer = pyb.Timer(timer, prescaler=0, period=65535)
+        # FIX PIN
+        self.timer.channel(chA, pyb.Timer.ENC_AB, pin=pyb.Pin.board.PB6)
+        # FIX PIN
+        self.timer.channel(chB, pyb.Timer.ENC_AB, pin=pyb.Pin.board.PB7) 
+        
+        # Initialize position and offset to 0
+        self.zero()          
 
     ## Gets the encoder's position
     #
     #  This method returns the current position of the encoder on the wheel of the motor.
     def read(self):
-        self.position = self.t8.counter()
+
+        prev_position = self.position
+        self.position = self.timer.counter() - self.offset
+        
+        delta = self.position - prev_position
+        if delta > 32767:
+            pass
+        elif delta < -32767:
+            pass
+        self.distance += delta
 
     ## Zeros out the encoder
     #
     #  Detailed info on encoder zero function
     def zero(self):
         self.position = 0;
+        self.distance = 0;
+        self.offset = self.timer.counter();
