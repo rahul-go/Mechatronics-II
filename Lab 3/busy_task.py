f9a3b7993a726888af3d9421c7b23844b4242bac
rahul-go <rahulxgoyal@gmail.com>
2019-01-31 12:40:52 -0800

    Fixed indentation

--- Lab 3/busy_task.py
+++ Lab 3/busy_task.py
@@ -14,30 +14,30 @@ import print_task
 
 
 class BusyTask:
-    """ Class which contains a task function. This tests the use of a class
-    method as a task function, and it allows lots of tasks to be easily made.
-    """
+	""" Class which contains a task function. This tests the use of a class
+	method as a task function, and it allows lots of tasks to be easily made.
+	"""
 
-    ## A serial number for @c BusyTask objects. This gives each created busy
-    #  task a separate identifier. This is an example of a variable which
-    #  "belongs to the class" rather than to any one instance of the class; 
-    #  note that it's accessed as @c BusyTask.t_num, not @c self.t_num
-    t_num = 0
+	## A serial number for @c BusyTask objects. This gives each created busy
+	#  task a separate identifier. This is an example of a variable which
+	#  "belongs to the class" rather than to any one instance of the class; 
+	#  note that it's accessed as @c BusyTask.t_num, not @c self.t_num
+	t_num = 0
 
-    def __init__ (self):
-        """ Initialize a busy task by creating its serial number. """
+	def __init__ (self):
+		""" Initialize a busy task by creating its serial number. """
 
-        ## The serial number of the busy task
-        self.ser_num = BusyTask.t_num
-        BusyTask.t_num += 1
+		## The serial number of the busy task
+		self.ser_num = BusyTask.t_num
+		BusyTask.t_num += 1
 
 
-    def run_fun (self):
-        """ Run function for the @c BusyTask.  This function doesn't do much
-        except to verify that a class member can be a task function. """
+	def run_fun (self):
+		""" Run function for the @c BusyTask.  This function doesn't do much
+		except to verify that a class member can be a task function. """
 
-        while True:
-            print_task.put ('[' + str (self.ser_num) + ']')
-            yield (0)
+		while True:
+			print_task.put ('[' + str (self.ser_num) + ']')
+			yield (0)
 
 
--- Lab 3/print_task.py
+++ Lab 3/print_task.py
@@ -44,67 +44,67 @@ PROFILE = True
 
 #@micropython.native
 def put (a_string):
-    """ Put a string into the print queue so it can be printed by the 
-    printing task whenever that task gets a chance. If the print queue is
-    full, characters are lost; this is better than blocking to wait for
-    space in the queue, as we'd block the printing task and space would
-    never open up. When a character has been put into the queue, the @c go()
-    method of the print task is called so that the run method will be called
-    as soon as the print task is run by the task scheduler. 
-    @param a_string A string to be put into the queue """
+	""" Put a string into the print queue so it can be printed by the 
+	printing task whenever that task gets a chance. If the print queue is
+	full, characters are lost; this is better than blocking to wait for
+	space in the queue, as we'd block the printing task and space would
+	never open up. When a character has been put into the queue, the @c go()
+	method of the print task is called so that the run method will be called
+	as soon as the print task is run by the task scheduler. 
+	@param a_string A string to be put into the queue """
 
-    for a_ch in a_string:
-        if not print_queue.full ():
-            print_queue.put (ord (a_ch))
-            print_task.go ()
+	for a_ch in a_string:
+		if not print_queue.full ():
+			print_queue.put (ord (a_ch))
+			print_task.go ()
 
 
 #@micropython.native
 def put_bytes (b_arr):
-    """ Put bytes from a @c bytearray or @c bytes into the print queue. When 
-    characters have been put into the queue, the @c go() method of the print
-    task is called so that the run method will be called as soon as the print 
-    task is run by the task scheduler. 
-    @param b_arr The bytearray whose contents go into the queue """
+	""" Put bytes from a @c bytearray or @c bytes into the print queue. When 
+	characters have been put into the queue, the @c go() method of the print
+	task is called so that the run method will be called as soon as the print 
+	task is run by the task scheduler. 
+	@param b_arr The bytearray whose contents go into the queue """
 
-    for byte in b_arr:
-        if not print_queue.full ():
-            print_queue.put (byte)
-            print_task.go ()
+	for byte in b_arr:
+		if not print_queue.full ():
+			print_queue.put (byte)
+			print_task.go ()
 
 
 def run ():
-    """ Run function for the task which prints stuff. This function checks for
-    any characters to be printed in the queue; if any characters are found 
-    then one character is printed, after which the print task yields so other 
-    tasks can run. This functino must be called periodically; the normal way 
-    is to make it the run function of a low priority task in a cooperatively 
-    multitasked system so that the task scheduler calls this function when 
-    the higher priority tasks don't need to run. 
-    """
+	""" Run function for the task which prints stuff. This function checks for
+	any characters to be printed in the queue; if any characters are found 
+	then one character is printed, after which the print task yields so other 
+	tasks can run. This functino must be called periodically; the normal way 
+	is to make it the run function of a low priority task in a cooperatively 
+	multitasked system so that the task scheduler calls this function when 
+	the higher priority tasks don't need to run. 
+	"""
 
-    while True:
-        # If there's a character in the queue, print it
-        if print_queue.any ():
-            print (chr (print_queue.get ()), end = '')
+	while True:
+		# If there's a character in the queue, print it
+		if print_queue.any ():
+			print (chr (print_queue.get ()), end = '')
 
-        # If there's another character, tell this task to run again ASAP
-        if print_queue.any ():
-            print_task.go ()
+		# If there's another character, tell this task to run again ASAP
+		if print_queue.any ():
+			print_task.go ()
 
-        yield (0)
+		yield (0)
 
 
 ## This queue holds characters to be printed when the print task gets around
 #  to it.
 global print_queue
 print_queue = task_share.Queue ('B', BUF_SIZE, name = "Print_Queue", 
-                        thread_protect = THREAD_PROTECT, overwrite = False)
+						thread_protect = THREAD_PROTECT, overwrite = False)
 
 ## This is the task which schedules printing. 
 global print_task
 print_task = cotask.Task (run, name = 'Printing', priority = 0, 
-                          profile = PROFILE)
+						  profile = PROFILE)
 
 # This line tells the task scheduler to add this task to the system task list
 cotask.task_list.append (print_task)
