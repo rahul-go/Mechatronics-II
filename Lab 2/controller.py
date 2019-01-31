002354e36e895ba4dd59ae9d46bb9382bf5e420e
ctkao2112 <46798149+ctkao2112@users.noreply.github.com>
2019-01-31 00:36:30 -0800

    Comments

--- Lab 2/controller.py
+++ Lab 2/controller.py
@@ -1,8 +1,13 @@
 ## @file controller.py
 #  The controller module contains the Controller class.
 #
-#  TODO
-#
+#  The controller class is a general proportional controller. The 
+#  initialization consists of setting a proportional gain and setpoint. The 
+#  controller must then be repeatedly called and given a measurement of the 
+#  value being controlled. The controller will then return a value to be 
+#  output to the controller. As an added functionality, the controller 
+#  records its time and output.
+#  
 #  @author Rahul Goyal, Cameron Kao, Harry Whinnery
 #
 #  @copyright License Info
@@ -13,7 +18,12 @@ import utime
 
 ## A controller object.
 #
-#  TODO
+#  The controller class is a general proportional controller. The 
+#  initialization consists of setting a proportional gain and setpoint. The 
+#  controller must then be repeatedly called and given a measurement of the 
+#  value being controlled. The controller will then return a value to be 
+#  output to the controller. As an added functionality, the controller 
+#  records its time and output.
 #  @author Rahul Goyal, Cameron Kao, and Harry Whinnery
 #  @copyright License Info
 #  @date January 31, 2019
@@ -21,13 +31,20 @@ class Controller:
 
 	## Constructor for the controller.
 	#
-	#  The constructor is called by TODO
+	#  The constructor is called by passing K_p and the setpoint. The 
+    #  constructor then saves these values as variables. It also creates empty 
+    #  arrays for the controller’s time and values.
 	#      
-	#  @param K_p TODO
+	#  @param K_p Proportional gain constant for controller. 
 	#
-	#  @param setpoint TODO
+	#  @param setpoint Initial sepoint for controller. Determines where the 
+    # controller will try to go when the run function is called. 
 	def __init__(self, K_p, setpoint):
 		print('Creating a controller!')
+		
+        ## Creates instance variables for K_p, setpoint, time and vals. Data 
+        #  will be later put into time and vals, where time is the time passed
+        #  and vals is the input measurement
 		self.K_p = K_p
 		self.setpoint = setpoint
 		self.time = []
@@ -35,33 +52,44 @@ class Controller:
 
 
 
-	## TODO
+	## Clears the data in instance variables time and vals
 	#
-	#  TODO
-	def clear_data(self):
+	#  The "clear_data" function is used whenever the user would like to clear
+    #  the existing time and vals variables
+	def clear_data(self):  
 		self.time = []
 		self.vals = []
 
 
 
-	## TODO
+	## Prints time and value data to the console
 	#
-	#  TODO
+	#  When the "get_data()" function is called, it offsets the time data 
+    #  by the inital value for time, such that the time data begins at 0. It 
+    #  then prints the time and value data to the console. 
 	def get_data(self):
-		t_0 = self.time[0]
-		
-		self.time = [t - self.time[0] for t in self.time]
-		data = zip(self.time, self.vals)
+		## Offsets time data by the initial value for time, such that the time
+        #  data begins at 0
+        self.time = [t - self.time[0] for t in self.time]
+		## Combines time and value lists into a list of lists, where each list 
+        # contains the time and corresponding value
+        data = zip(self.time, self.vals)
+        # Prints each value in the data list to the console.
 		for datum in data:
 			print(datum)
 
 
 
-	## TODO
+	## Returns output value to actuator
 	#
-	#  TODO
+	#  The "run()" function passes the sensor measurement and outputs an 
+    #  actuation value based on the K_p and setpoint. The avtuation value 
+    #  saturates at -100 and 100.
 	def run(self, measurement):
-		self.time.append(utime.ticks_ms())
+		
+        ## Adds the current time, as read by utime.ticks_ms() to the end of the 
+        # time list
+        self.time.append(utime.ticks_ms())
 		self.vals.append(measurement)
 
 		actuation = self.K_p * (self.setpoint - measurement)
