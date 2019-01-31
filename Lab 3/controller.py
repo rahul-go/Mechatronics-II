0d9b455399def542c3c8ebb283ebe6e8ea03e749
ctkao2112 <46798149+ctkao2112@users.noreply.github.com>
2019-01-31 14:17:19 -0800

    Started motor1 task

--- Lab 3/controller.py
+++ Lab 3/controller.py
@@ -32,19 +32,19 @@ class Controller:
 	## Constructor for the controller.
 	#
 	#  The constructor is called by passing K_p and the setpoint. The 
-    #  constructor then saves these values as variables. It also creates empty 
-    #  arrays for the controller’s time and values.
+	#  constructor then saves these values as variables. It also creates empty 
+	#  arrays for the controller’s time and values.
 	#      
 	#  @param K_p Proportional gain constant for controller. 
 	#
 	#  @param setpoint Initial sepoint for controller. Determines where the 
-    # controller will try to go when the run function is called. 
+	# controller will try to go when the run function is called. 
 	def __init__(self, K_p=1, setpoint=0):
 		print('Creating a controller!')
 		
-        ## Creates instance variables for K_p, setpoint, time and vals. Data 
-        #  will be later put into time and vals, where time is the time passed
-        #  and vals is the input measurement.
+		## Creates instance variables for K_p, setpoint, time and vals. Data 
+		#  will be later put into time and vals, where time is the time passed
+		#  and vals is the input measurement.
 		self.K_p = K_p
 		self.setpoint = setpoint
 		self.time = []
@@ -55,7 +55,7 @@ class Controller:
 	## Clears the data in instance variables time and vals.
 	#
 	#  The "clear_data" function is used whenever the user would like to clear
-    #  the existing time and vals variables.
+	#  the existing time and vals variables.
 	def clear_data(self):  
 		self.time = []
 		self.vals = []
@@ -65,16 +65,16 @@ class Controller:
 	## Prints time and value data to the console.
 	#
 	#  When the "get_data()" function is called, it offsets the time data 
-    #  by the inital value for time, such that the time data begins at 0. It 
-    #  then prints the time and value data to the console. 
+	#  by the inital value for time, such that the time data begins at 0. It 
+	#  then prints the time and value data to the console. 
 	def get_data(self):
 		## Offsets time data by the initial value for time, such that the time
-        #  data begins at 0.
-        self.time = [t - self.time[0] for t in self.time]
+		#  data begins at 0.
+		self.time = [t - self.time[0] for t in self.time]
 		## Combines time and value lists into a list of lists, where each list 
-        #  contains the time and corresponding value.
-        data = zip(self.time, self.vals)
-        #  Prints each value in the data list to the console.
+		#  contains the time and corresponding value.
+		data = zip(self.time, self.vals)
+		#  Prints each value in the data list to the console.
 		for datum in data:
 			print(str(datum[0]) + ', ' + str(datum[1]))
 
@@ -83,22 +83,22 @@ class Controller:
 	## Returns output value to actuator.
 	#
 	#  The "run()" function passes the sensor measurement and outputs an 
-    #  actuation value based on the K_p and setpoint. The avtuation value 
-    #  saturates at -100 and 100.
+	#  actuation value based on the K_p and setpoint. The avtuation value 
+	#  saturates at -100 and 100.
 	def run(self, measurement):
 		
-        ## Adds the current time, as read by utime.ticks_ms() to the end of the 
-        #  time list.
-        self.time.append(utime.ticks_ms())
+		## Adds the current time, as read by utime.ticks_ms() to the end of the 
+		#  time list.
+		self.time.append(utime.ticks_ms())
 		
 		## Adds the current measurement to the end of the vals list.
 		self.vals.append(measurement)
 
-        ## The actuaion value is the difference between the setpoint and 
-        #  measurement values multiplied by the K_p value. If this value is 
-        #  greater than 100, it is set to 100. If it is less than -100 it is 
-        #  set to -100. This saturation prevents the controller from returning
-        #  an actuation value that is too extreme.
+		## The actuaion value is the difference between the setpoint and 
+		#  measurement values multiplied by the K_p value. If this value is 
+		#  greater than 100, it is set to 100. If it is less than -100 it is 
+		#  set to -100. This saturation prevents the controller from returning
+		#  an actuation value that is too extreme.
 		actuation = self.K_p * (self.setpoint - measurement)
 		if actuation > 100:
 			return 100
--- Lab 3/main.py
+++ Lab 3/main.py
@@ -7,30 +7,50 @@
 #
 #  @date February 07, 2019
 
-import pyb
 import micropython
-import gc
+import pyb
+import utime
 
-import cotask
-import task_share
-import print_task
+import controller
+import encoder
+import motor
+
 import busy_task
+import cotask
+import print_task
+import task_share
+
+
 
 # Allocate memory so that exceptions raised in interrupt service routines can
 # generate useful diagnostic printouts
 micropython.alloc_emergency_exception_buf (100)
 
-def motor1_fun():
 
-	## A motor object
-	pinENA = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
-	pinIN1A = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-	pinIN2A = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
-	motor = motor.MotorDriver([pinIN1A, pinIN2A, pinENA], 3, [1, 2])
-	## An encoder object
-	encoder = encoder.Encoder([pyb.Pin.board.PB6, pyb.Pin.board.PB7], 4, [1, 2])
-	## A controller object
-	controller = controller.Controller(K_p=0.10)
+
+## A motor object
+pinENA = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
+pinIN1A = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+pinIN2A = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
+motor1 = motor.MotorDriver([pinIN1A, pinIN2A, pinENA], 3, [1, 2])
+## An encoder object
+encoder1 = encoder.Encoder([pyb.Pin.board.PB6, pyb.Pin.board.PB7], 4, [1, 2])
+## A controller object
+controller1 = controller.Controller(K_p=0.10)
+
+def motor1_fun():
+	# Step response
+	while(True):
+		controller1.clear_data()
+		input('Press "enter" to run a step response test!')
+		encoder1.zero()
+		controller1.set_setpoint(encoder1.read() + 4000)
+		stop = utime.ticks_add(utime.ticks_ms(), 1000)
+	while(utime.ticks_diff(stop, utime.ticks_ms()) > 0):
+		motor1.set_duty_cycle(controller1.run(encoder1.read()))
+		utime.sleep_ms(10)
+	motor1.set_duty_cycle(0)
+	controller1.get_data()
 
 
 
@@ -43,18 +63,3 @@ def motor1_fun():
 motor1_task = cotask.Task(motor1_fun, name = 'Motor 1', priority = 1,
 						  period = 1000, profile = True, trace = False)
 cotask.task_list.append(motor1_task)
-
-
-
-# Step response
-while(True):
-	controller.clear_data()
-	input('Press "enter" to run a step response test!')
-	encoder.zero()
-	controller.set_setpoint(encoder.read() + 4000)
-	stop = utime.ticks_add(utime.ticks_ms(), 1000)
-	while(utime.ticks_diff(stop, utime.ticks_ms()) > 0):
-		motor.set_duty_cycle(controller.run(encoder.read()))
-		utime.sleep_ms(10)
-	motor.set_duty_cycle(0)
-	controller.get_data()
