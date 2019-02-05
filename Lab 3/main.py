bc7bc2329900468960c633e174d1c8e281f56508
ctkao2112 <46798149+ctkao2112@users.noreply.github.com>
2019-02-04 17:09:23 -0800

    Task state implementation (sort of)
    I'm sorry. This is probably wrong.

--- Lab 3/main.py
+++ Lab 3/main.py
@@ -26,28 +26,39 @@ import busy_task
 # generate useful diagnostic printouts
 micropython.alloc_emergency_exception_buf (100)
 
+
+vcp = pyb.USB_VCP ()
+
 # TODO
 def motor1_fun():
-
+	## A motor object
+	pinENA = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
+	pinIN1A = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+	pinIN2A = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
+	motor = motor.MotorDriver([pinIN1A, pinIN2A, pinENA], 3, [1, 2])
+	## An encoder object
+	encoder = encoder.Encoder([pyb.Pin.board.PB6, pyb.Pin.board.PB7], 4, [1, 2])
+	## A controller object
+	controller = controller.Controller(K_p=0.10)
+	## A motor controller object
+	motorcontroller = motorcontroller.MotorController(motor, encoder, controller)
 	state = 0
-	while(True)
+
+	while(True):
 		if state == 0:
-			## A motor object
-			pinENA = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP)
-			pinIN1A = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-			pinIN2A = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
-			motor = motor.MotorDriver([pinIN1A, pinIN2A, pinENA], 3, [1, 2])
-			## An encoder object
-			encoder = encoder.Encoder([pyb.Pin.board.PB6, pyb.Pin.board.PB7], 4, [1, 2])
-			## A controller object
-			controller = controller.Controller(K_p=0.10)
-			## A motor controller object
-			motorcontroller = motorcontroller.MotorController(motor, encoder, controller)
+			# If key is pressed, set state to 1
+			if vcp.read() == b'\x31':
+				state = 1
+
+
 		elif state == 1:
 			# Step response
-			input('Press "enter" to run a step response test!')
 			motorcontroller1.step_response();
+			
+			# Figure out how and when to set state back to 0
+			state = 0
 
+		yield (state)
 
 
 # =============================================================================
@@ -56,6 +67,19 @@ def motor1_fun():
 # for state transition tracing, and the application will run out of memory
 # after a while and quit. Therefore, use tracing only for debugging and set
 # trace to False when it's not needed.
+share0 = task_share.Share ('i', thread_protect = False, name = "Share_0")
 motor1_task = cotask.Task(motor1_fun, name = 'Motor 1', priority = 1,
 						  period = 1000, profile = True, trace = False)
 cotask.task_list.append(motor1_task)
+
+
+
+
+while True:
+	cotask.task_list.pri_sched ()
+
+# Print a table of task data and a table of shared information data
+print ('\n' + str (cotask.task_list) + '\n')
+print (task_share.show_all ())
+print (task1.get_trace ())
+print ('\r\n')
